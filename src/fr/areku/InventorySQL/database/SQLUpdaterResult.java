bf5467b0b83c16eb5861c750a4602308a473ca83
Cue <cue@loadingchunks.net>
2013-03-08 12:29:16 +0000

    Pomalom

--- src/fr/areku/InventorySQL/database/SQLUpdaterResult.java
+++
@@ -1,5 +0,0 @@
-package fr.areku.InventorySQL.database;
-
-public interface SQLUpdaterResult {
-	void updateResult(boolean success);
-}
--- src/fr/areku/InventorySQL/schemas/schema_users.sql
+++
@@ -1,4 +0,0 @@
-ALTER IGNORE TABLE `%%TABLENAME%%` ADD `name` VARCHAR( 32 ) NOT NULL AFTER `id`;
-ALTER IGNORE TABLE `%%TABLENAME%%` ADD `password` VARCHAR( 32 ) NOT NULL AFTER `name`;
-DELETE FROM `%%TABLENAME%%` USING `%%TABLENAME%%_users`, `%%TABLENAME%%_users` as vtable WHERE (NOT `%%TABLENAME%%_users`.ID=vtable.ID) AND (`%%TABLENAME%%_users`.name=vtable.name);
-ALTER IGNORE TABLE `%%TABLENAME%%` ADD UNIQUE `name` ( `name` ) ;
